f3b3a258e69e79bdea340565199406d46c3113cc
Ashutosh <vu4f1920027@pvppcoe.ac.in>
2021-03-21 21:11:26 +0530

    project-2 day 2 completed

---
+++ Cat-Generator/main.js
@@ -0,0 +1,6 @@
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('cat-gen');
+    image.src = 'tenor.gif';
+    div.appendChild(image);
+}
